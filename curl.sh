fbc88c9319e106fd77c504f7ff09c64e28c9babb
EyePatchMorty <95339737+EyePatchMorty@users.noreply.github.com>
2023-05-07 17:13:07 +0800

    Update curl.sh

--- curl.sh
+++ curl.sh
@@ -1,10 +1,9 @@
-curl -x 127.0.0.1:8118 --include \
-     --no-buffer \
-     --header "Connection: Upgrade" \
-     --header "Upgrade: websocket" \
-     --header "Host: flying-dragonfly.as.r.appspot.com" \
-     --header "Sec-WebSocket-Key: BhrmQYkMFcuVZkONXcYOCw==" \
-     --header "Sec-WebSocket-Version: 13" \
+curl -i -N  -x 127.0.0.1:8080  \
+     -H "Connection: Upgrade" \
+     -H "Upgrade: websocket" \
+     -H "Host: flying-dragonfly.as.r.appspot.com" \
+     -H "Sec-WebSocket-Key: BhrmQYkMFcuVZkONXcYOCw==" \
+     -H "Sec-WebSocket-Version: 13" \
+     -H "user-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36" \
      http://flying-dragonfly.as.r.appspot.com/echo
 
- 
